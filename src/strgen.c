1be25f2aeb3539f23fdb8dfad40d331f6355ff98
John Call <johnkc1992@gmail.com>
2017-01-28 10:57:23 -0700

    more type consistency and resolve some compiler warnings

--- src/strgen.c
+++ src/strgen.c
@@ -1,8 +1,8 @@
 #include "strgen.h"
 
-unsigned int power(unsigned int a, unsigned int b) {
-	unsigned int out = 1;
-	unsigned int square = a*a;
+uint power(uint a, uint b) {
+	uint out = 1;
+	uint square = a*a;
 	while (b > 1) {
 		out *= square;
 		b -= 2;
@@ -13,20 +13,20 @@ unsigned int power(unsigned int a, unsigned int b) {
 	return out;
 }
 
-unsigned int loop_alpha(unsigned int n, byte*** out) {
+uint loop_alpha(uint n, byte*** out) {
 	// Compute the size we need for out: N = 26 ^ n
-	unsigned int size;
+	uint size;
 	size = power(26, n);
 
 	// Create a list of size N
 	byte** list = malloc(sizeof(char*) * size);
 	for (uint i = 0; i < size; i++) {
-		list[i] = malloc(sizeof(char) * (n + 1)); // +1 for \0 (easier debugging)
+		list[i] = (byte*)malloc(sizeof(char) * (n + 1)); // +1 for \0 (easier debugging)
 	}
 
-	byte* str = malloc(sizeof(char) * (n + 1)); // +1 for \0 (easier debugging)
+	byte* str = (byte*)malloc(sizeof(char) * (n + 1)); // +1 for \0 (easier debugging)
 
-	int i;
+	uint i;
 	for (i = 0; i < n; i++) {  // sets initial value to all a
 		str[i] = 'a';
 	}
@@ -35,7 +35,7 @@ unsigned int loop_alpha(unsigned int n, byte*** out) {
 	//increment at digit
 	// if > 'z' set digit = 'a' and increment digit - 1
 	// digit < 0 break
-	unsigned int digit = n - 1;
+	uint digit = n - 1;
 
 	i = 0;
 	memcpy(list[i++], str, n + 1); // +1 for \0 (easier debugging)
