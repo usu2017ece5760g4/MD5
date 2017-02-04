7e09a6eaa7a86752f27dad0114c957d5710f124b
John Call <johnkc1992@gmail.com>
2017-02-04 15:34:01 -0700

    remove call to deprecated function for gpu architecture

--- src/main.cu
+++ src/main.cu
@@ -7,14 +7,13 @@
 //---------------------------------------------------------------------------------------------------------------------+
 #include <stdlib.h>
 #include <stdio.h>
-#include "strgen.h"
 #include "benchtime.h"
 
 int main(int argc, char **argv) {
 	const int N = 5;
 
 	byte* list;
-	uint size = loop_alpha(N, &list);
+	uint size = 0; // loop_alpha(N, &list);
 
 	for (uint i = 0; i < size; ++i) {
 		printf("The hash of %.*s is: ", N, (list + (i * N)));
