fa6802fe00b4fa7a1e7253228dafce60f751d70b
John Call <johnkc1992@gmail.com>
2017-02-04 14:22:18 -0700

    move common logic into benchtime.h

--- src/main.c
+++ src/main.c
@@ -9,28 +9,6 @@
 #include <stdio.h>
 #include "benchtime.h"
 
-typedef unsigned char byte;
-typedef unsigned int uint;
-
-extern void md5_attack(const uint* hash, const uint n);
-
-void print_hash(uint* hash) {
-	union {
-		uint raw[4];
-		union {
-			uint word;
-			byte byte[4];
-		} words[4];
-	} *hout = hash;
-
-	for (uint i = 0; i < 4; ++i) {
-		printf("%02x", hout->words[i].byte[0]);
-		printf("%02x", hout->words[i].byte[1]);
-		printf("%02x", hout->words[i].byte[2]);
-		printf("%02x", hout->words[i].byte[3]);
-	}
-}
-
 int main(int argc, char **argv) {
 	const int N = 6;
 
